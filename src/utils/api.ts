01c80a34411dc16c7938713fb487336f8b94a052
H Usman Ahmad <hafizahmad022@gmail.com>
2024-12-18 01:10:07 +0500

    link change

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://desole-project-backend-9eflv3g0v-usmans-projects-a4a4fef2.vercel.app/api",
+  baseURL: "https://backend-vercel-blue.vercel.app/api",
 });
 
 export default api;
